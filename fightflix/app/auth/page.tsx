9be17d215343fc1a836c644d4ae0e6278a23b894
Julian Jong <julian.jong74@gmail.com>
2024-08-01 21:15:44 +0800

    fix: Position logo in top left

--- fightflix/app/auth/page.tsx
+++ fightflix/app/auth/page.tsx
@@ -3,7 +3,7 @@ export default function Page() {
         <div className="relative h-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
             <div className="bg-black w-full h-full lg:bg-opacity-50">
                 <nav className="px-10 py-5">
-                    <img src="/images/logo.png" alt="logo" className="h-48 w-96" />
+                    <img src="/images/logo.png" alt="logo" className="object-left-top h-12" />
                 </nav>
                 <div className="flex justify-center">
                     <div className="bg-black bg-opacity-80 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
