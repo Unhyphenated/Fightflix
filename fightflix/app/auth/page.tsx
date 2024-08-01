522e37b07ef665a1b646fd9ee4f84a1a901e0aaf
Julian Jong <julian.jong74@gmail.com>
2024-08-01 20:42:46 +0800

    add: Landing page background

---
+++ fightflix/app/auth/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <div className="relative h-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+            
+        </div>
+    )
+}
