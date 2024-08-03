a8cc6fcbd7a41b449e9980851527542e6fbb8ad0
Julian Jong <julian.jong74@gmail.com>
2024-08-03 21:03:39 +0800

    add: Input field for email

--- fightflix/app/auth/page.tsx
+++ fightflix/app/auth/page.tsx
@@ -1,3 +1,5 @@
+import Input from "@/app/components/Input";
+
 export default function Page() {
     return (
         <div className="relative h-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -10,6 +12,9 @@ export default function Page() {
                         <h2 className="text-white text-4xl mb-8 font-semibold">
                             Sign In
                         </h2>
+                        <div className="flex flex-col gap-4">
+                            <Input />
+                        </div>
                     </div>
                 </div>
             </div>
