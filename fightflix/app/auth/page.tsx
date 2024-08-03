446bc9242a506f474ed11dc046414303db318ce1
Julian Jong <julian.jong74@gmail.com>
2024-08-03 21:30:38 +0800

    add: Change login screen according to user status

--- fightflix/app/auth/page.tsx
+++ fightflix/app/auth/page.tsx
@@ -12,7 +12,7 @@ export default function Page() {
     const toggleVariant = useCallback(() => {
         setVariant((currentVariant) => currentVariant === "login" ? "register" : "login");
     }, [])
-    
+
     return (
         <div className="relative h-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
             <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -22,9 +22,10 @@ export default function Page() {
                 <div className="flex justify-center">
                     <div className="bg-black bg-opacity-80 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
                         <h2 className="text-white text-4xl mb-8 font-semibold">
-                            Sign In
+                            {variant === 'login' ? "Sign In" : "Register"}
                         </h2>
                         <div className="flex flex-col gap-4">
+                            {variant === 'register' && (
                             <Input 
                                 label="Username"
                                 onChange={(e: any) => setName(e.target.value)}
@@ -32,6 +33,7 @@ export default function Page() {
                                 type="username"
                                 value={name}
                             />
+                            )}
                             <Input 
                                 label="Email"
                                 onChange={(e: any) => setEmail(e.target.value)}
@@ -48,12 +50,12 @@ export default function Page() {
                             />
                         </div>
                         <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
-                            Login
+                            {variant === 'login' ? 'Login' : 'Sign Up'}
                         </button>
                         <p className="text-neutral-500 mt-12">
-                            Using Fightflix for the first time?
-                            <span className="text-white ml-1 hover:underline cursor-pointer">
-                                Create new account
+                            {variant === "login" ? "Using Fightflix for the first time?" : "Already have an account?"}
+                            <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
+                                {variant === "login" ? "Create an account" : "Login"}
                             </span>
                         </p>
                     </div>
