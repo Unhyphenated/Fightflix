2b2b1f629297e755570186274c964904c5f48657
Julian Jong <julian.jong74@gmail.com>
2024-08-03 21:20:40 +0800

    add: Username, email, password forms

--- fightflix/app/auth/page.tsx
+++ fightflix/app/auth/page.tsx
@@ -1,6 +1,18 @@
+'use client';
+import { useCallback, useState } from "react";
 import Input from "@/app/components/Input";
 
 export default function Page() {
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [variant, setVariant] = useState('login');
+
+    const toggleVariant = useCallback(() => {
+        setVariant((currentVariant) => currentVariant === "login" ? "register" : "login");
+    }, [])
+    
     return (
         <div className="relative h-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
             <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -13,8 +25,37 @@ export default function Page() {
                             Sign In
                         </h2>
                         <div className="flex flex-col gap-4">
-                            <Input />
+                            <Input 
+                                label="Username"
+                                onChange={(e: any) => setName(e.target.value)}
+                                id="name"
+                                type="username"
+                                value={name}
+                            />
+                            <Input 
+                                label="Email"
+                                onChange={(e: any) => setEmail(e.target.value)}
+                                id="email"
+                                type="email"
+                                value={email}
+                            />
+                            <Input 
+                                label="Password"
+                                onChange={(e: any) => setPassword(e.target.value)}
+                                id="password"
+                                type="password"
+                                value={password}
+                            />
                         </div>
+                        <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                            Login
+                        </button>
+                        <p className="text-neutral-500 mt-12">
+                            Using Fightflix for the first time?
+                            <span className="text-white ml-1 hover:underline cursor-pointer">
+                                Create new account
+                            </span>
+                        </p>
                     </div>
                 </div>
             </div>
