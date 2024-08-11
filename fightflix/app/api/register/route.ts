bebbabf119d2b957d8847a6169eb1b764826f41f
Julian Jong <julian.jong74@gmail.com>
2024-08-11 20:58:03 +0800

    fix: Rename register file to route

---
+++ fightflix/app/api/register/route.ts
@@ -0,0 +1,34 @@
+import bcrypt from 'bcrypt';
+import { NextRequest, NextResponse } from 'next/server';
+import prismadb from '@/lib/prismadb';
+
+export async function POST(req: NextRequest) {
+    try {
+        const { email, name, password } = await req.json();
+
+        const existingUser = await prismadb.user.findUnique({
+            where: {
+                email,
+            }
+        });
+
+        if (existingUser) {
+            return NextResponse.json({ error: 'Email taken' }, { status: 422 });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12);
+        const user = await prismadb.user.create({
+            data: {
+                email,
+                name,
+                hashedPassword,
+                image: '',
+                emailVerified: new Date(),
+            }
+        });
+        return NextResponse.json(user, { status: 200 });
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({ error: 'Something went wrong' }, { status: 400 });
+    }
+}
