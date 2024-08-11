8fc8d3de4a90ab37616e878a4381de397a60f119
Julian Jong <julian.jong74@gmail.com>
2024-08-11 21:44:17 +0800

    fix: Registration connection to Prisma

--- fightflix/app/api/register/route.ts
+++ fightflix/app/api/register/route.ts
@@ -1,21 +1,23 @@
 import bcrypt from 'bcrypt';
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import prismadb from '@/lib/prismadb';
 
-export async function POST(req: NextRequest) {
+export async function POST(req: Request) {
     try {
         const { email, name, password } = await req.json();
+        const users = await prismadb.user.findMany();
+        console.log(users); 
 
+        // Check if the user already exists
         const existingUser = await prismadb.user.findUnique({
-            where: {
-                email,
-            }
+            where: { email },
         });
 
         if (existingUser) {
             return NextResponse.json({ error: 'Email taken' }, { status: 422 });
         }
 
+        // Hash the password and create a new user
         const hashedPassword = await bcrypt.hash(password, 12);
         const user = await prismadb.user.create({
             data: {
@@ -24,11 +26,13 @@ export async function POST(req: NextRequest) {
                 hashedPassword,
                 image: '',
                 emailVerified: new Date(),
-            }
+            },
         });
+
+        console.log('User created:', user);
         return NextResponse.json(user, { status: 200 });
     } catch (error) {
-        console.log(error);
-        return NextResponse.json({ error: 'Something went wrong' }, { status: 400 });
+        console.error('Error during registration:', error);
+        return NextResponse.json({ error: 'Something went wrong' }, { status: 500 });
     }
 }
