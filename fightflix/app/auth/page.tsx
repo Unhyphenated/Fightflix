fc88e38c8ea53ad1792e799d7711cf6ec9db0d19
Julian Jong <julian.jong74@gmail.com>
2024-08-12 21:04:55 +0800

    add: Github and Google logins

--- fightflix/app/auth/page.tsx
+++ fightflix/app/auth/page.tsx
@@ -5,6 +5,9 @@ import axios from 'axios';
 import {signIn} from 'next-auth/react';
 import { useRouter } from "next/navigation";
 
+import { FcGoogle } from 'react-icons/fc';
+import { FaGithub } from 'react-icons/fa';
+
 export default function Page() {
     const router = useRouter();
     const [email, setEmail] = useState('');
@@ -82,6 +85,38 @@ export default function Page() {
                         <button onClick={variant === 'login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
                             {variant === 'login' ? 'Login' : 'Sign Up'}
                         </button>
+
+                        <div className="flex flex-row items-center gap-4, mt-8 justify-center">
+                            <div
+                                className="
+                                w-10
+                                h-10
+                                bg-white
+                                rounded-full
+                                flex
+                                items-center
+                                justify-center
+                                cursor-pointer
+                                hover:opacity-80
+                                transition">
+                                <FcGoogle size={30}/>
+                            </div>
+                            <div
+                                className="
+                                w-10
+                                h-10
+                                bg-white
+                                rounded-full
+                                flex
+                                items-center
+                                justify-center
+                                cursor-pointer
+                                hover:opacity-80
+                                transition">
+                                <FaGithub size={30}/>
+                            </div>
+                        </div>
+
                         <p className="text-neutral-500 mt-12">
                             {variant === "login" ? "Using Fightflix for the first time?" : "Already have an account?"}
                             <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
