5132dc703e5f33ec39fdb47dd97e8a76bc616db4
Julian Jong <julian.jong74@gmail.com>
2024-08-17 22:57:00 +0800

    add: Hover effect when cursor on fight card

--- fightflix/components/FightCard.tsx
+++ fightflix/components/FightCard.tsx
@@ -23,6 +23,35 @@ const FightCard: React.FC<FightCardProps> = ({
                 
                 "
             src={data.thumbnailUrl} alt="Thumbnail"/>
+            <div 
+                className="
+                opacity-0
+                absolute
+                top-0
+                transition
+                duration-200
+                z-10
+                invisible
+                sm:visible
+                delay-300
+                w-full
+                scale-0
+                group-hover:scale-110
+                group-hover:-translate-y-[6vw]
+                group-hover:translate-x-[2vw]
+                group-hover:opacity-100
+                ">
+                    <img className="
+                        cursor-pointer
+                        object-cover
+                        transition
+                        duration
+                        shadow-xl
+                        rounded-t-md
+                        w-full
+                    "
+                    src={data.thumbnailUrl} alt="Thumbnail"/>
+                </div>
         </div>
     )
 }
