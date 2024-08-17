bc5bd4921838761fec182eee3383fdcef3bb336c
Julian Jong <julian.jong74@gmail.com>
2024-08-17 22:09:08 +0800

    fix: Quality settings for video

--- fightflix/components/Billboard.tsx
+++ fightflix/components/Billboard.tsx
@@ -9,7 +9,7 @@ const Billboard = () => {
     <div className="relative w-full" style={{ paddingTop: '56.25%' }}>
         <iframe
             className="absolute top-0 left-0 w-full h-full object-cover brightness-[60%]"
-            src={`https://www.youtube.com/embed/${data?.videoId}?autoplay=1&mute=1&loop=1&playlist=${data?.videoId}&vq=hd720 or &vq=hd1080&controls=0&disablekb=1&modestbranding=1`}
+            src={`https://www.youtube.com/embed/${data?.videoId}?autoplay=1&mute=1&loop=1&playlist=${data?.videoId}&vq=hd1080&controls=0&disablekb=1&modestbranding=1`}
             frameBorder="0"
             allow="autoplay; encrypted-media"
             allowFullScreen
