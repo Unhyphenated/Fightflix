6566be1fb2ea9ade3325e86d97188e519d3083e4
Julian Jong <julian.jong74@gmail.com>
2024-08-17 21:56:59 +0800

    add: More info button

--- fightflix/components/Billboard.tsx
+++ fightflix/components/Billboard.tsx
@@ -1,5 +1,6 @@
 import useBillboard from "@/hooks/useBillboard";
 import React from "react";
+import { CiCircleInfo } from "react-icons/ci";
 
 const Billboard = () => {
     const { data } = useBillboard();
@@ -8,7 +9,7 @@ const Billboard = () => {
     <div className="relative w-full" style={{ paddingTop: '56.25%' }}>
         <iframe
             className="absolute top-0 left-0 w-full h-full object-cover brightness-[60%]"
-            src={`https://www.youtube.com/embed/${data?.videoId}?autoplay=1&mute=1&loop=1&playlist=${data?.videoId}&vq=hd1080&controls=0&disablekb=1&modestbranding=1`}
+            src={`https://www.youtube.com/embed/${data?.videoId}?autoplay=1&mute=1&loop=1&playlist=${data?.videoId}&vq=hd720 or &vq=hd1080&controls=0&disablekb=1&modestbranding=1`}
             frameBorder="0"
             allow="autoplay; encrypted-media"
             allowFullScreen
@@ -19,8 +20,26 @@ const Billboard = () => {
                 <p className="text-white text-1xl md:text-5xl h-full w-[50%] lg:text-6xl font-bold drop-shadow-xl">
                     {data?.title}
                 </p>
-                <div className="flex flex-row items-center mt-3">
-                    
+                <div className="flex flex-row items-center mt-3 md:mt-4 gap-3">
+                    <button
+                        className="
+                            bg-white
+                            text-white
+                            bg-opacity-30
+                            rounded-md
+                            py-1 md:py-2
+                            px-2 md:px-4
+                            w-auto
+                            text-xs lg:text-lg
+                            font-semibold
+                            flex
+                            flex-row
+                            items-center
+                            hover:bg-opacity-20
+                            transition">
+                            <CiCircleInfo className="mr-2"/>
+                            Info
+                    </button>
                 </div>
             </div>
         </div>
