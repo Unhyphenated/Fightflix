bcfb126d8b041be3211053c06638b2d75b6fb8ff
Julian Jong <julian.jong74@gmail.com>
2024-08-17 22:09:21 +0800

    add: Obtain fights from prisma

---
+++ fightflix/pages/api/fights/index.ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import prismadb from '@/lib/prismadb';
+import serverAuth from "@/lib/serverAuth";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method !== 'GET') {
+        return res.status(405).end();
+    }
+
+    try {
+        await serverAuth(req);
+        const fights = await prismadb.fight.findMany();
+        return res.status(200).end();
+    } catch (error) {
+        console.log(error);
+        return res.status(400).end();
+    }
+}
