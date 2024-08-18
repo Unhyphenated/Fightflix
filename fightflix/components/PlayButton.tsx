95a12c935b98a045c08579cba316c0811b28f98c
Julian Jong <julian.jong74@gmail.com>
2024-08-18 14:04:53 +0800

    add: Play button styling

--- fightflix/components/PlayButton.tsx
+++ fightflix/components/PlayButton.tsx
@@ -11,8 +11,22 @@ const PlayButton: React.FC<PlayButtonProps> = ({ fightId }) => {
     const router = useRouter();
 
     return (
-        <div>
-
+        <div className="
+            bg-white
+            rounded-md
+            py-1 md:py-2
+            px-2 md:px-4
+            w-auto
+            text-xs lg:text-lg
+            font-semibold
+            flex
+            flex-row
+            items-center
+            hover:bg-neutral-300
+            transition
+            ">
+                <IoPlay size={25} className="mr-1"/>
+                Play
         </div>
     )
 };
