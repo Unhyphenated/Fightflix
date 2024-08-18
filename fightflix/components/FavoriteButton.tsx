18fe26fbf674c7316ae5bff616e487a075db4105
Julian Jong <julian.jong74@gmail.com>
2024-08-18 13:51:01 +0800

    add: Toggle favorites

--- fightflix/components/FavoriteButton.tsx
+++ fightflix/components/FavoriteButton.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React from "react";
+import React, { useCallback, useMemo } from "react";
 
 import useCurrentUser from "@/hooks/useCurrentUser";
 import useFavorites from "@/hooks/useFavorites";
@@ -10,6 +10,28 @@ interface FavoriteButtonProps {
 }
 
 const FavoriteButton: React.FC<FavoriteButtonProps> = ({ fightId }) => {
+    const { mutate: mutateFavorites } = useFavorites();
+    const { data: currentUser, mutate } = useCurrentUser();
+
+    const isFavorite = useMemo(() => {
+        const list = currentUser?.favoriteIds || [];
+        return list.includes(fightId);
+    }, [currentUser, fightId]);
+
+    const toggleFavorites = useCallback(async () => {
+        let response;
+        if (isFavorite) {
+            response = await axios.delete('/api/favorite', { data: { fightId }})
+        } else {
+            response = await axios.post('/api/favorite', { fightId })
+        }
+        const updatedFavoriteIds = response?.data?.favoriteIds;
+        mutate({
+            ...currentUser,
+            favoriteIds: updatedFavoriteIds;
+        })
+    }, [fightId, isFavorite, currentUser, mutate, mutateFavorites]);
+
     return (
         <div className="
             cursor-pointer
